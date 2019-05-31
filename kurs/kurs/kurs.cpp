2d7f3d19092d8bcd8c3d4da4657c910fff80c6d4
Ilya <nesterovsusu@gmail.com>
2019-05-31 16:39:00 +0500

    kurs base
    .

--- kurs/kurs/kurs.cpp
+++ kurs/kurs/kurs.cpp
@@ -1,24 +1,98 @@
 ﻿#include "pch.h"
-#include <iostream>
+#include <iostream> 
 #include <string>
+//#include <queue>
 
 using namespace std;
 
-class Kandidat {
-private:
+class Node {
 	string name;
-	int birth;
-	string place;
-	int index;
+	unsigned int year;
+	string city;
+	unsigned int prior;
 public:
-	Kandidat(string name, int birth, string place) {
-		this->name = name;
-		this->birth = birth;
-		this->place = place;
+	Node() {
+		name = "";
+		year = 0;
+		city = "";
+		prior = 0;
 	}
+	Node(string name, unsigned int year, string city, unsigned int prior) : name(name), year(year), city(city), prior(prior) {
+	}
+	friend ostream& operator<<(ostream& os, Node& n)
+	{
+		return os << "FIO - " << n.name << " Year - " << n.year << " City - " << n.city << " Prior - " << n.prior;
+	}
+};
+
+
+template<class T>
+class Queue {
+	struct Elem {
+		T obj;
+		Elem* next;
+	};
+	Elem* head;
+	Elem* last;
+	Elem* iterator;
+public:
+	Queue() : head(nullptr) {}
+
+	void push(T val) {
+		if (head == nullptr) {
+			head = new Elem;
+			head->obj = val;
+			head->next = nullptr;
+			last = head;
+		}
+		else {
+			last->next = new Elem;
+			last->next->obj = val;
+			last->next->next = nullptr;
+			last = last->next;
+		}
+	}
+	void pop() {
+		if (head == nullptr) {
+			cout << "Queue is empty";
+		}
+		else {
+			head = head->next;
+		}
+	}
+
+	void begin() {
+		iterator = head;
+	}
+	void next()
+	{
+		iterator = iterator->next;
+	}
+
+	T& getCurrent()
+	{
+		return iterator->obj;
+	}
+
+	bool isEnd()
+	{
+		return iterator == nullptr;
+	}
+
 };
 
 int main()
 {
-	
+	Queue<Node> q;
+	q.push(Node("p", 123, "a", 2));
+	q.push(Node("q", 124, "b", 2));
+	q.push(Node("w", 125, "c", 2));
+	for (q.begin(); !q.isEnd(); q.next()) {
+		cout << q.getCurrent() << endl;
+	}
+	q.pop();
+	cout << endl;
+	for (q.begin(); !q.isEnd(); q.next()) {
+		cout << q.getCurrent() << endl;
+	}
 }
