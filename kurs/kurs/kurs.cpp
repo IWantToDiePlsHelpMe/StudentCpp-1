83115ac630fbd8aa7491a979c693486fa99f413e
Ilya <nesterovsusu@gmail.com>
2019-06-02 19:04:06 +0500

    file stream almost done
    .

--- kurs/kurs/kurs.cpp
+++ kurs/kurs/kurs.cpp
@@ -1,23 +1,23 @@
 ﻿#include "pch.h"
 #include <iostream> 
 #include <string>
-//#include <queue>
+#include <fstream>
 
 using namespace std;
 
 class Node {
 	string name;
-	unsigned int year;
+	string year;
 	string city;
 	unsigned int prior;
 public:
 	Node() {
 		name = "";
-		year = 0;
+		year = "";
 		city = "";
 		prior = 0;
 	}
-	Node(string name, unsigned int year, string city, unsigned int prior) : name(name), year(year), city(city), prior(prior) {
+	Node(string name, string year, string city, unsigned int prior) : name(name), year(year), city(city), prior(prior) {
 	}
 	friend ostream& operator<<(ostream& os, Node& n)
 	{
@@ -58,9 +58,9 @@ public:
 		}
 		else {
 			head = head->next;
+			
 		}
 	}
-
 	void begin() {
 		iterator = head;
 	}
@@ -68,31 +68,59 @@ public:
 	{
 		iterator = iterator->next;
 	}
-
 	T& getCurrent()
 	{
 		return iterator->obj;
 	}
-
 	bool isEnd()
 	{
 		return iterator == nullptr;
 	}
-
 };
 
 int main()
 {
 	Queue<Node> q;
-	q.push(Node("p", 123, "a", 2));
-	q.push(Node("q", 124, "b", 2));
-	q.push(Node("w", 125, "c", 2));
-	for (q.begin(); !q.isEnd(); q.next()) {
-		cout << q.getCurrent() << endl;
+	int choice;
+	bool f = true;
+	string name, city, year, str;
+	unsigned int  prior;
+	fstream fout;
+	ifstream file_check("file.txt", ios::in);
+	if (file_check.peek() != ifstream::traits_type::eof()) { //возможно нужно изменить доработать для считывания строки но не слова
+		while (!file_check.eof()) {
+			file_check >> name;
+			file_check >> year;
+			file_check >> city;
+			file_check >> prior;
+			q.push(Node(name, year, city, prior));
+		}
+		
 	}
-	q.pop();
-	cout << endl;
-	for (q.begin(); !q.isEnd(); q.next()) {
-		cout << q.getCurrent() << endl;
-	}
+	cout << "Choose what you want to do 1add 2delete 3show 4break: ";
+	do {
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			cout << "Enter application" << endl;
+			fout.open("file.txt", ios::out | ios::app);
+			cin >> name;
+			cin >> year;
+			cin >> city;
+			cin >> prior;
+			cout << endl;
+			fout << name << endl << year << endl << city << endl << prior << endl;
+			q.push(Node(name, year, city, prior));
+			fout.close();
+			break;
+		case 2:
+			q.pop();
+			break;
+		case 3:
+			for (q.begin(); !q.isEnd(); q.next()) cout << q.getCurrent() << endl;
+			break;
+		case 4:
+			return 0;
+		}
+	} while (choice !=  4);
 }
